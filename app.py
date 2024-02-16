de3676df00c404f6832944248560899ed62524e8
MateusMarquesDias <103127371+MateusMarquesDias@users.noreply.github.com>
2024-02-15 22:42:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import socket
 import redis
 import logging
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -10,9 +11,16 @@ def get_ip():
     try:
         nome_host = socket.gethostname()
         endereco_ip = socket.gethostbyname(nome_host)
+        mysql.connector.connect(
+          host="localhost",
+          user="root",
+          password=""
+        )
     except socket.gaierror:
         logging.error("Falha ao recuperar o endereço IP: Nome do host não pôde ser resolvido")
         endereco_ip = 'Não foi possível recuperar o endereço IP'
+    except mysql.connector.Error as err:
+        logging.error(f"Erro ao conectar com o banco de dados: {err.msg}")
     except socket.error as e:
         logging.error(f"Ocorreu um erro de soquete: {e}")
         endereco_ip = 'Não foi possível recuperar o endereço IP'
